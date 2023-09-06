feb7381cf095434dc705f00ac683eddfd2d9e64c
okanaytimur <okanaytimur@gmail.com>
2023-09-06 16:25:36 +0300

    Toplama fonksiyonu eklendi

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -1,2 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+
+int sayi1,sayi2;
+
+Console.WriteLine("Sayi bir gir : ");
+sayi1=Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Sayi iki gir : ");
+sayi2=Convert.ToInt32(Console.ReadLine());
+
+int toplam;
+toplam=sayi1+sayi2;
+Console.WriteLine(toplam.ToString());
